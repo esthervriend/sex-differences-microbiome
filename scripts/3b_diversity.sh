1f2caa8d504b1b844c5715be7ac2c7f802d2a6e4
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2023-12-30 15:42:00 +0100

    load conda env, remove flag Rscript command

--- scripts/3b_diversity.sh
+++ scripts/3b_diversity.sh
@@ -3,4 +3,6 @@
 #SBATCH --mem=10G
 #SBATCH --time=12:00:00
 #SBATCH -p 'genoa'
-Rscript -e /projects/0/prjs0784/sex-differences-microbiome/scripts/3a_diversityplots.R
+eval "$(conda shell.bash hook)"
+conda activate rscripts
+Rscript /projects/0/prjs0784/sex-differences-microbiome/scripts/3a_diversityplots.R
--- scripts/4b_ordination.sh
+++ scripts/4b_ordination.sh
@@ -3,4 +3,6 @@
 #SBATCH --mem=16G
 #SBATCH --time=12:00:00
 #SBATCH -p 'genoa'
-Rscript -e /projects/0/prjs0784/sex-differences-microbiome/scripts/4a_ordinationplots.R
+eval "$(conda shell.bash hook)"
+conda activate rscripts
+Rscript /projects/0/prjs0784/sex-differences-microbiome/scripts/4a_ordinationplots.R
