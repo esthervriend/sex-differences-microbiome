e1822a08ccff58100c3632e7ad6ca60744fef466
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2023-12-30 13:30:12 +0100

    root tree

--- scripts/1_data_cleaning.R
+++ scripts/1_data_cleaning.R
@@ -2,9 +2,9 @@
 
 ## Libraries
 library(tidyverse)
-library(rio) # kun je alles mee openen zonder te definieren wat voor bestand het is
+library(rio) 
 library(haven)
-library(phyloseq) # voor handling van phyloseq object
+library(phyloseq)
 
 ## Open phyloseq object
 otu <- rio::import("data/HELUIS.Verhaar.2023.otu_table.csv", header = TRUE)
@@ -15,10 +15,11 @@ rownames(tax) <- tax$V1
 tax$V1 <- NULL
 tax <- as.matrix(tax)
 tree <- ape::read.tree("data/HELUIS.Verhaar.2023.phy_tree.tree")
+tree_rooted <- ape::root(tree)
 # sampledata <- rio::import("data/HELUIS.Verhaar.2023.sam_data.csv", header = TRUE) = empty
 
 ## Make phyloseq
-heliusmb <- phyloseq(otu_table(otu, taxa_are_rows = T), tax_table(tax), phy_tree(tree))
+heliusmb <- phyloseq(otu_table(otu, taxa_are_rows = T), tax_table(tax), phy_tree(tree_rooted))
 sample_names(heliusmb) <-str_c("S",sample_names(heliusmb)) # safeguard that sample names cannot be used as numeric in matrices
 sample_names(heliusmb)
 
