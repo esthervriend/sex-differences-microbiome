e0f0e67fb3e4adf6a8f041d9795eb7fee260160d
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2024-02-04 12:52:32 +0100

    update with new folder names

--- scripts/7_process_mlresults.R
+++ scripts/7_process_mlresults.R
@@ -1,13 +1,13 @@
 ## Process results machine learning
 
-library(dplyr)
+library(tidyverse)
 library(ggplot2)
 library(ggsci)
 library(stringr)
 
 source("scripts/functions.R")
 
-path_true <- 'male_female/output_XGB_class_male_female_2023_12_20__22-56-43'
+path_true <- 'male_female/output_XGB_class_male_female_2024_01_26__23-56-15'
 path_permuted <- 'male_female/output_XGB_class_male_female_2023_12_22__20-25-22_PERMUTED'
 data_path <- 'male_female/input_data'
 labels <- c("Male", "Female")
@@ -17,8 +17,8 @@ plot_feature_importance_class(path_true, 20)
 plot_features_tests_class(data_path, path_true, top_n=20, labels)
 plot_features_tests_top(data_path, path_true, top_n=20, nrow=4, labels)
 
-path_true <- 'menopause/output_XGB_class_menopause_2023_12_20__22-57-00'
-path_permuted <- 'menopause/output_XGB_class_menopause_2023_12_21__20-51-36_PERMUTED'
+path_true <- 'menopause/output_XGB_class_menopause_2024_01_24__10-09-15'
+path_permuted <- 'menopause/output_XGB_class_menopause_2024_01_26__23-56-32_PERMUTED'
 data_path <- 'menopause/input_data'
 labels <- c("Postmenopausal", "Premenopausal")
 
