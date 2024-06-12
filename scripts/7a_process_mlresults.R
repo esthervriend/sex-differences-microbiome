ca13c46853c7ca243ab60d4e34f05a17a397b754
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2024-06-12 11:27:01 +0200

    labels men-women, save as png

--- scripts/7a_process_mlresults.R
+++ scripts/7a_process_mlresults.R
@@ -11,7 +11,7 @@ source("scripts/functions.R")
 path_true <- 'male_female/output_XGB_class_male_female_2024_01_26__23-56-15'
 path_permuted <- 'male_female/output_XGB_class_male_female_2023_12_22__20-25-22_PERMUTED'
 data_path <- 'male_female/input_data'
-labels <- c("Male", "Female")
+labels <- c("Men", "Women")
 
 compared_to_permuted_class(path_true, path_permuted)
 
@@ -23,6 +23,8 @@ plarr1 <- ggarrange(ggarrange(svg_grob), pl2, pl3,
                     nrow = 3, labels = c("A", "B", "C"), 
                     heights = c(1.2,1.3,0.8))
 ggsave(plarr1, filename = "results/16s_comp_sex.pdf",
+       width = 14, height = 18)
+ggsave(plarr1, filename = "results/16s_comp_sex.png",
        width = 14, height = 18)
 
 ## Plot assembled figure composition menopause
@@ -41,4 +43,6 @@ plarr1 <- ggarrange(ggarrange(svg_grob), pl2, pl3,
                     nrow = 3, labels = c("A", "B", "C"), 
                     heights = c(1.2,1.3,0.8))
 ggsave(plarr1, filename = "results/16s_comp_meno.pdf",
+       width = 14, height = 18)
+ggsave(plarr1, filename = "results/16s_comp_meno.png",
        width = 14, height = 18)
