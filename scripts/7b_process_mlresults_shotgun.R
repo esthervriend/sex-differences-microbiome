1c192744d941d8a05e37750a97d781d2daf45903
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2024-06-12 17:17:55 +0200

    add plot differences menopause, split men <50 >50

--- scripts/7b_process_mlresults_shotgun.R
+++ scripts/7b_process_mlresults_shotgun.R
@@ -78,12 +78,13 @@ plot_features_top_shotgun(data_path, path_true, top_n=20, nrow=4, labels)
 pl2 <- plot_feature_importance_shotgun(path_true, 20)
 grConvert::convertPicture(file.path(path_true,"Plot_AUC.pdf"), file.path(path_true,"auc.svg"))
 svg_grob <- svgparser::read_svg(file.path(path_true,"auc.svg"))
-plarr1 <- ggarrange(ggarrange(svg_grob), pl2, 
-                    nrow = 2, labels = c("A", "B"), heights = c(1.0,1.0))
+pl3 <- plot_features_top_shotgun(data_path, path_true, top_n=5, nrow = 1, labels)
+plarr1 <- ggarrange(ggarrange(svg_grob), pl2, pl3,
+                    nrow = 3, labels = c("A", "B", "C"), heights = c(1.2,1.3,0.8))
 ggsave(plarr1, filename = "results/comp_menopause.pdf",
-       width = 12, height = 15)
+       width = 14, height = 15)
 ggsave(plarr1, filename = "results/comp_menopause.png",
-       width = 12, height = 15)
+       width = 14, height = 15)
 
 
 #### PATHWAYS ####
@@ -102,34 +103,36 @@ pathwaysofinterest <- c("PPGPPMET-PWY", "PHOSLIPSYN-PWY", "PYRIDOXSYN-PWY", "PWY
 pathwaysfilt <- pathwaysfilt %>% filter(rownames(.) %in% pathwaysofinterest)
 pyri <- as.data.frame(t(as.matrix(pathwaysfilt)))
 pyri$ID <- rownames(pyri)
-pyri <- pyri %>% select(everything(.), "ppGp metabolism" = `PPGPPMET-PWY`,
+pyri <- pyri %>% dplyr::select(everything(.), "ppGp metabolism" = `PPGPPMET-PWY`,
                         "pyridoxal 5'−phosphate biosynthesis I" = `PYRIDOXSYN-PWY`,
                         "superpathway of phospholipid biosynthesis I (bacteria)" = `PHOSLIPSYN-PWY`,
                         "L−histidine degradation III" = `PWY-5030`)
 
 dfpaths <- inner_join(df, pyri, by = "ID")
 dfpaths <- dfpaths %>% mutate(Menopause_Sex = case_when(
-    Sex == "Men" ~ "Men",
+    Sex == "Men" & Age < 50 ~ "Men <50",
+    Sex == "Men" & Age >= 50 ~ "Men >50",
     MenopauseYn == "Postmenopausal" ~ "Postmenopausal",
     MenopauseYn == "Premenopausal" ~ "Premenopausal"
 ),
 Menopause_Sex = as.factor(Menopause_Sex),
-Menopause_Sex = fct_relevel(Menopause_Sex, "Postmenopausal", after = 2L)
+Menopause_Sex = fct_relevel(Menopause_Sex, "Postmenopausal", after = 3L),
+Menopause_Sex = fct_relevel(Menopause_Sex, "Younger men", after = 0L)
 )
 
 pathli <- list()
-for(path in names(dfpaths)[58:61]){
+for(path in names(dfpaths)[59:62]){
     dfpaths$pathid <- dfpaths[[path]]
     print(path)
     lab_y <- log10(max(dfpaths$pathid))*1.05
-    comp <- list(c("Men", "Premenopausal"), c("Premenopausal", "Postmenopausal"),
-                 c("Men", "Postmenopausal"))
+    comp <- list(c("Men <50", "Men >50"), c("Men <50", "Premenopausal"), c("Premenopausal", "Postmenopausal"),
+                 c("Men >50", "Postmenopausal"))
     pl <- ggplot(data = dfpaths, aes(x = Menopause_Sex, 
                                      y = pathid)) +
         scale_y_log10()+
-        geom_violin(aes(fill = MenopauseYn)) +
+        geom_violin(aes(fill = Menopause_Sex)) +
         geom_boxplot(fill = "white", width = 0.2, outlier.shape = NA) +
-        scale_fill_manual(values = pal_nejm()(4)[c(4,3,2)], guide = "none") +
+        scale_fill_manual(values = pal_nejm()(4)[c(4,3,2,1)], guide = "none") +
         stat_compare_means(label.y = lab_y, comparisons = comp, label = "p.signif",
                            step.increase = 0.07, tip.length = 0) +
         labs(fill = "", x = "", y = "Relative abundance (cpm)", title = path) +
@@ -145,7 +148,7 @@ ggsave("results/diffpathways_menopause.pdf", width = 10, height = 12)
 ggsave("results/diffpathways_menopause.png", width = 10, height = 12)
 
 pathli2 <- list()
-for(path in names(dfpaths)[58:61]){
+for(path in names(dfpaths)[59:62]){
     dfpaths$pathid <- dfpaths[[path]]
     print(path)
     lab_y <- log10(max(dfpaths$pathid))*1.2
