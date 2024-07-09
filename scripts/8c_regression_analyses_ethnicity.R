89b30e73c3b18bfb4770073b82fea57277232881
esthervriend <153101447+esthervriend@users.noreply.github.com>
2024-07-09 14:40:32 +0200

    Add files via upload

--- scripts/8c_regression_analyses_ethnicity.R
+++ scripts/8c_regression_analyses_ethnicity.R
@@ -32,8 +32,8 @@ pred_sex <- pred_sex %>% slice_head(n = 20)
 Modelsex <- data.frame()
 for (i in 1:nrow(pred_sex)) {
   var_name <- pred_sex$FeatName[i]
-  formula <- as.formula(paste("log(", var_name, "+1) ~ Sex * Ethnicity"))
-  coef <- data.frame(var_name, summary(lm(formula, data = dfcomplete))$coef[c(9,10,11,12,13,14), c(1, 2, 4)])
+  formula <- as.formula(paste("log(", var_name, "+1) ~ Sex * Ethnicity + Age + BMI + HT + DM + CurrSmoking"))
+  coef <- data.frame(var_name, summary(lm(formula, data = dfcomplete))$coef[c(14,15,16,17,18,19), c(1, 2, 4)])
   Modelsex <- bind_rows(Modelsex, coef)
 }
 Modelsex <- as.data.frame(Modelsex)
@@ -50,8 +50,8 @@ pred_meno <- pred_meno %>% slice_head(n = 20)
 Modelmenopause <- data.frame()
 for (i in 1:nrow(pred_meno)) {
   var_name <- pred_meno$FeatName[i]
-  formula <- as.formula(paste("log(", var_name, "+1) ~ MenopauseYn * Ethnicity"))
-  coef <- data.frame(var_name, summary(lm(formula, data = dfcomplete))$coef[c(9,10,11,12,13,14), c(1, 2, 4)])
+  formula <- as.formula(paste("log(", var_name, "+1) ~ MenopauseYn * Ethnicity + Age + BMI + HT + DM + CurrSmoking"))
+  coef <- data.frame(var_name, summary(lm(formula, data = dfcomplete))$coef[c(14,15,16,17,18,19), c(1, 2, 4)])
   Modelmenopause <- bind_rows(Modelmenopause, coef)
 }
 Modelmenopause <- as.data.frame(Modelmenopause)
